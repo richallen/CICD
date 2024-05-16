9be7ad2ce45861e7bfb78ab58a2bd6cad3ab1b03
oilulio <oilulio@users.noreply.github.com>
2024-05-16 20:05:57 +0100

    Create flask_server.py

---
+++ flask_server.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+import datetime
+import json
+from flask import Flask
+from waitress import serve
+from flask import Response
+
+applicationName="Application Test 1"
+hostName="0.0.0.0"
+PORT=9000
+
+data={applicationName:[{"version":"1.0","description":"pre-interview technical test","lastcommitsha":"abc57858585","UTC":datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d %H:%M:%S')}]}
+
+app = Flask(__name__)
+
+@app.route('/healthcheck')
+def healthcheck():
+    data[applicationName][0]["UTC"]=datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d %H:%M:%S')
+    return (Response(json.dumps(data),mimetype="application/vnd.api+json"))
+
+if __name__ == "__main__":
+  print("Server started http://%s:%s"%(hostName,PORT))
+  serve(app,host=hostName,port=PORT)
+  print("Server stopped")
