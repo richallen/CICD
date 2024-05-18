38314904f4f6ad732a5bf48704c5e95ccf2ccd53
oilulio <oilulio@users.noreply.github.com>
2024-05-18 08:19:57 +0100

    Update linted flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -5,20 +5,32 @@ from flask import Flask
 from waitress import serve
 from flask import Response
 
-applicationName="Application Test 1"
-hostName="0.0.0.0"
-PORT=9000
+applicationName = "Application Test 1"
+hostName = "0.0.0.0"
+PORT = 9000
 
-data={applicationName:[{"version":"1.0","description":"pre-interview technical test","lastcommitsha":"abc57858585","UTC":datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d %H:%M:%S')}]}
+data = {
+    applicationName: [
+        {
+            "version": "1.0",
+            "description": "pre-interview technical test",
+            "lastcommitsha": "abc57858585",
+            "UTC": "",
+        }
+    ]
+}
 
 app = Flask(__name__)
 
 @app.route('/healthcheck')
+
 def healthcheck():
-    data[applicationName][0]["UTC"]=datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d %H:%M:%S')
-    return (Response(json.dumps(data),mimetype="application/vnd.api+json"))
+    data[applicationName][0]["UTC"] = datetime.datetime.now(datetime.UTC).strftime(
+        '%Y-%m-%d %H:%M:%S'
+    )
+    return (Response(json.dumps(data),mimetype = "application/vnd.api+json"))
 
 if __name__ == "__main__":
-  print("Server started http://%s:%s"%(hostName,PORT))
-  serve(app,host=hostName,port=PORT)
+  print("Server started http://%s:%s"%(hostName, PORT))
+  serve(app,host = hostName,port = PORT)
   print("Server stopped")
